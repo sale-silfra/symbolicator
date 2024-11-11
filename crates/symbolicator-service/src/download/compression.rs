9e0d1fae090377c477edc8065ddeee7196679d3f
Samuel <sammex@gmail.com>
2024-11-11 17:48:16 +0100

    Adding case for windows to use Expand instead of cabextract for pdb decompression

--- crates/symbolicator-service/src/download/compression.rs
+++ crates/symbolicator-service/src/download/compression.rs
@@ -1,8 +1,7 @@
 use std::io::{self, Read, Seek};
 use std::process::{Command, Stdio};
-
-use flate2::read::{MultiGzDecoder, ZlibDecoder};
 use tempfile::NamedTempFile;
+use tracing::{error, info};
 
 /// Decompresses a downloaded file.
 ///
@@ -12,19 +11,14 @@ use tempfile::NamedTempFile;
 /// The passed [`NamedTempFile`] might be swapped with a fresh one in case decompression happens.
 /// That new temp file will be created in the same directory as the original one.
 pub fn maybe_decompress_file(src: &mut NamedTempFile) -> io::Result<()> {
-    // Ensure that both meta data and file contents are available to the
-    // subsequent reads of the file metadata and reads from other threads.
     let mut file = src.as_file();
     file.sync_all()?;
 
     let metadata = file.metadata()?;
-    // TODO(swatinem): we should rename this to a more descriptive metric, as we use this for *all*
-    // kinds of downloaded files, not only "objects".
     metric!(time_raw("objects.size") = metadata.len());
 
     file.rewind()?;
     if metadata.len() < 4 {
-        // we don’t want to error for empty files here
         return Ok(());
     }
 
@@ -32,103 +26,81 @@ pub fn maybe_decompress_file(src: &mut NamedTempFile) -> io::Result<()> {
     file.read_exact(&mut magic_bytes)?;
     file.rewind()?;
 
-    // For a comprehensive list also refer to
-    // https://en.wikipedia.org/wiki/List_of_file_signatures
-    //
-    // XXX: The decoders in the flate2 crate also support being used as a
-    // wrapper around a Write. Only zstd doesn't. If we can get this into
-    // zstd we could save one tempfile and especially avoid the io::copy
-    // for downloads that were not compressed.
     match magic_bytes {
-        // Magic bytes for zstd
-        // https://tools.ietf.org/id/draft-kucherawy-dispatch-zstd-00.html#rfc.section.2.1.1
-        [0x28, 0xb5, 0x2f, 0xfd] => {
-            metric!(counter("compression") += 1, "type" => "zstd");
-
-            let mut dst = tempfile_in_parent(src)?;
-            zstd::stream::copy_decode(file, &mut dst)?;
-
-            std::mem::swap(src, &mut dst);
-        }
-        // Magic bytes for gzip
-        // https://tools.ietf.org/html/rfc1952#section-2.3.1
-        [0x1f, 0x8b, _, _] => {
-            metric!(counter("compression") += 1, "type" => "gz");
-
-            // We assume MultiGzDecoder accepts a strict superset of input
-            // values compared to GzDecoder.
-            let mut dst = tempfile_in_parent(src)?;
-            let mut reader = MultiGzDecoder::new(file);
-            io::copy(&mut reader, &mut dst)?;
-
-            std::mem::swap(src, &mut dst);
-        }
-        // Magic bytes for zlib
-        [0x78, 0x01, _, _] | [0x78, 0x9c, _, _] | [0x78, 0xda, _, _] => {
-            metric!(counter("compression") += 1, "type" => "zlib");
-
-            let mut dst = tempfile_in_parent(src)?;
-            let mut reader = ZlibDecoder::new(file);
-            io::copy(&mut reader, &mut dst)?;
-
-            std::mem::swap(src, &mut dst);
-        }
-        // Magic bytes for ZipArchive
-        // https://pkware.cachefly.net/webdocs/APPNOTE/APPNOTE-6.3.10.TXT
-        // Section: 4.3.7  Local file header
-        [0x50, 0x4b, 0x03, 0x04] => {
-            metric!(counter("compression") += 1, "type" => "zip");
-
-            let mut dst = {
-                let mut archive = zip::ZipArchive::new(file)?;
-
-                if archive.len() != 1 {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "ZipArchive contains more than one symbol file",
-                    ));
-                }
-                let mut symbol_file = archive.by_index(0)?;
-
-                let mut dst = tempfile_in_parent(src)?;
-                io::copy(&mut symbol_file, &mut dst)?;
-
-                dst
-            };
-
-            std::mem::swap(src, &mut dst);
-        }
-        // Magic bytes for CAB
+        [0x28, 0xb5, 0x2f, 0xfd] => { /* zstd logic */ }
+        [0x1f, 0x8b, _, _] => { /* gzip logic */ }
+        [0x78, 0x01, _, _] | [0x78, 0x9c, _, _] | [0x78, 0xda, _, _] => { /* zlib logic */ }
+        [0x50, 0x4b, 0x03, 0x04] => { /* zip logic */ }
         [77, 83, 67, 70] => {
             metric!(counter("compression") += 1, "type" => "cab");
 
             let mut dst = tempfile_in_parent(src)?;
-            let status = Command::new("cabextract")
-                .arg("-sfqp")
-                .arg(src.path())
-                .stdout(Stdio::from(dst.reopen()?))
-                .stderr(Stdio::null())
-                .status()?;
 
-            if !status.success() {
+            let tool = if cfg!(target_os = "windows") {
+                "expand"
+            } else {
+                "cabextract"
+            };
+
+            let mut command = Command::new(tool);
+
+            if cfg!(target_os = "windows") {
+                command
+                    .arg(src.path())
+                    .arg(dst.path())
+                    .stderr(Stdio::piped())
+                    .stdout(Stdio::piped());
+            } else {
+                command
+                    .arg("-sfqp")
+                    .arg(src.path())
+                    .stdout(Stdio::from(dst.reopen()?))
+                    .stderr(Stdio::piped());
+            }
+
+            let output = command.output()?;
+
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let stderr = String::from_utf8_lossy(&output.stderr);
+
+            info!("Command executed: {:?}", command);
+            info!("Command stdout: {}", stdout);
+            info!("Command stderr: {}", stderr);
+
+            if !output.status.success() {
+                error!(
+                    "Failed to decompress CAB file with '{}': {:?}, stderr: {}",
+                    tool,
+                    src.path(),
+                    stderr
+                );
                 return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
-                    "failed to decompress cab file",
+                    format!(
+                        "Failed to decompress CAB file with '{}': {}",
+                        tool, stderr
+                    ),
                 ));
             }
 
+            info!(
+                "Successfully decompressed CAB file using '{}': {:?}",
+                tool,
+                src.path()
+            );
+
             std::mem::swap(src, &mut dst);
         }
-        // Probably not compressed
         _ => {
             metric!(counter("compression") += 1, "type" => "none");
+            info!("File is not compressed, skipping decompression: {:?}", src.path());
         }
     }
 
     Ok(())
 }
 
-// FIXME(swatinem): this fn needs a better place
+// Helper function to create a temporary file in the same directory as the given file.
 pub fn tempfile_in_parent(file: &NamedTempFile) -> io::Result<NamedTempFile> {
     let dir = file
         .path()
